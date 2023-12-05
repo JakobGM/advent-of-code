c459c9e7a98cfc5bfca5a0a7ba660e681cfbcc8f
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2023-12-05 22:37:10 +0100

    2024-05b Python: First solution

--- 2023/python/day5.py
+++ 2023/python/day5.py
@@ -1,53 +1,65 @@
-from collections.abc import Callable
+import itertools
 import math
 from pathlib import Path
-
 
 PROBLEM = Path("input/5.txt").read_text().strip()
 SEEDS, *SPECS = PROBLEM.split("\n\n")
 SEEDS = list(map(int, SEEDS.partition(": ")[2].split()))
 
 
-def create_mapping(
-    source_start: int,
-    destination_start: int,
-) -> Callable[[int], int]:
-    def mapping(source: int) -> int:
-        return destination_start + (source - source_start)
+class Mapping:
+    def __init__(self, line: str) -> None:
+        self.destination_start, self.source_start, self.height = map(int, line.split())
+        self.domain = range(self.source_start, self.source_start + self.height)
 
-    return mapping
+    def step(self, source: int) -> tuple[int, int]:
+        target = self.destination_start + (source - self.source_start)
+        remaining_height = self.height - (source - self.source_start)
+        return (target, remaining_height)
+
+    def applicable_for(self, source: int) -> bool:
+        return source in self.domain
 
 
-layers = []
+LAYERS = []
 for spec in SPECS:
     header, *tables = spec.split("\n")
     layer = []
     for table in tables:
-        destination_start, source_start, length = map(int, table.split())
-        applicable_for = range(source_start, source_start + length)
-        mapping = create_mapping(
-            source_start=source_start,
-            destination_start=destination_start,
-        )
-        layer.append((applicable_for, mapping))
-    layers.append(layer)
-
-NUM_LAYERS = len(layers)
+        mapping = Mapping(table)
+        layer.append(mapping)
+    LAYERS.append(layer)
 
 
-def traverse(seed: int) -> int:
+def traverse(seed: int) -> tuple[int, int]:
     position = seed
-    for layer in layers:
+    remaining_height = math.inf
+    for layer in LAYERS:
         try:
-            mapping = next(m for a, m in layer if position in a)
-            position = mapping(position)
+            mapping = next(
+                mapping for mapping in layer if mapping.applicable_for(position)
+            )
+            position, new_height = mapping.step(position)
+            remaining_height = min(remaining_height, new_height)
         except StopIteration:
-            position = position
-    return position
+            pass
+    return position, remaining_height
 
 
+# --- Part One ---
 min_position = math.inf
 for seed in SEEDS:
-    position = traverse(seed=seed)
+    position, _ = traverse(seed=seed)
     min_position = min(min_position, position)
+print(min_position)
+
+# -- Part Two ---
+min_position = math.inf
+for seed_start, seed_length in itertools.batched(SEEDS, n=2):
+    seed_range = range(seed_start, seed_start + seed_length)
+    seed = seed_start
+    while seed in seed_range:
+        position, height = traverse(seed=seed)
+        min_position = min(min_position, position)
+        seed += height
 print(min_position)
